4ba4338db3f1f70d7447eb95195ac597bf3be77c
Dilys-web <opokudonkord@gmail.com>
2024-05-20 15:01:34 +0000

    feat:error response exception

---
+++ src/main/java/com/employee/Employee/Management/System/exception/ErrorResponse.java
@@ -0,0 +1,15 @@
+package com.employee.Employee.Management.System.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+
+@Data
+@AllArgsConstructor
+public class ErrorResponse {
+    private  HttpStatus status;
+    private  String message;
+    private  String timestamp; // Optional
+
+}
