a954fff400bffeb6d30a736b8bad705bf7ffd980
Dilys-web <opokudonkord@gmail.com>
2024-05-29 16:11:26 +0000

    mod: modified employeedto

--- src/main/java/com/employee/Employee/Management/System/dto/EmployeeDto.java
+++ src/main/java/com/employee/Employee/Management/System/dto/EmployeeDto.java
@@ -1,18 +1,23 @@
 package com.employee.Employee.Management.System.dto;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.*;
 
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class EmployeeDto {
     private Long id;
+
+    @NotEmpty(message = "First name should not be empty")
     private String firstName;
+    @NotEmpty(message = "Second name should not be empty")
     private String secondName;
+    @NotEmpty(message = "Email should not be empty")
+    @Email
     private String email;
 }
