106fab54cfa6e3b97f43705822784b2928fa1fda
Dilys-web <opokudonkord@gmail.com>
2024-05-29 16:21:07 +0000

    feat: Implement employee search by email, first name, and second name

--- src/main/java/com/employee/Employee/Management/System/repository/EmployeeRepository.java
+++ src/main/java/com/employee/Employee/Management/System/repository/EmployeeRepository.java
@@ -3,7 +3,15 @@ package com.employee.Employee.Management.System.repository;
 import com.employee.Employee.Management.System.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 // type of entity and type of primary key are passed
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    Optional<Employee> findByEmail(String email);
+
+    List<Employee> findByFirstNameContaining(String firstName);
+    List<Employee> findBySecondNameContaining(String secondName);
 
 }
