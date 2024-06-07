0e293dd84f2a790f2ace83e1226124dc56369280
Dilys-web <opokudonkord@gmail.com>
2024-06-07 12:22:14 +0000

    feat: Entity for managing Paystack payment information

---
+++ src/main/java/com/employee/Employee/Management/System/entity/PaymentPaystack.java
@@ -0,0 +1,63 @@
+package com.employee.Employee.Management.System.entity;
+
+import com.employee.Employee.Management.System.entity.Employee;
+import com.employee.Employee.Management.System.enums.PricingPlanType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name= "paystack_payment")
+public class PaymentPaystack {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    private Employee user;
+
+    @Column(name = "reference")
+    private String reference;
+
+    @Column(name = "amount")
+    private BigDecimal amount;
+
+    @Column(name = "gateway_response")
+    private String gatewayResponse;
+
+    @Column(name = "paid_at")
+    private String paidAt;
+
+    @Column(name = "created_at")
+    private String createdAt;
+
+    @Column(name = "channel")
+    private String channel;
+
+    @Column(name = "currency")
+    private String currency;
+
+    @Column(name = "ip_address")
+    private String ipAddress;
+
+    @Column(name = "pricing_plan_type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private PricingPlanType pricingPlanType = PricingPlanType.BASIC;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_on", updatable = false, nullable = false)
+    private Date createdOn;
+}
